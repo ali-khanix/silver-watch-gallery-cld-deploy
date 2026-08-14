cca1247659ee746c96428456414e8c66782e6ae4
Ali Khani <103037526+ali-khanix@users.noreply.github.com>
2026-08-15 02:33:04 +0330

    disable image optimazation for local uploads

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
+      { protocol: "https", hostname: "**" },
+      { protocol: "http", hostname: "**" },
     ],
   },
 };
